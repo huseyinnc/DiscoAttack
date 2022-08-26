a8a0d650ee6b31fea7a8562598265a526343f986
dark-dragon001 <rafi.khudayar@gmail.com>
2022-08-26 08:11:24 -0400

    DiscoAttack

--- one.py
+++ one.py
@@ -1,7 +1,6 @@
-from cmath import pi
-from operator import truediv
-import nmap
 
+import nmap
+import os
 
 nm = nmap.PortScanner()
 
@@ -13,31 +12,44 @@ print ("Thank you for yor using this tool.")
 print ("##################################################################################################################\n")
 
 
-##############################################################################################
-# The network discovery part starts here.
-##############################################################################################
-
-
-
-
-
-
-ip = input("Please enter an ip address range: ")
-print ("Network range acquired successfully.\n")
 print ("##################################################################################################################")
 print ("Scan type: pingScan, osScan, udpScan")
 print ("##################################################################################################################\n")
 
-userInput = input("Please define the type of scanning: ")
+userInput = input("Please specify the type of scanning: ")
 
+# Nmap ping scan function, with out put to outputs repo in .csv format.
 def pingScan():
-  print(nm.scan(hosts = ip, arguments = "-sn"))
+  print (userInput, "selected.\n")
+  ip = input("Please enter an ip address range: ")
+  print ("Network range acquired successfully.\n")
+  print ("Attempting Ping scan...")
+  nm.scan(hosts = ip, arguments='-sn')
+  hosts_list = [(x, nm[x]['status']['state']) for x in nm.all_hosts()]
+  with open("outputs/pingscanresult.csv", "w") as scanout:
+    for host, status in hosts_list:
+      print(('{0}:{1}'.format(host, status)), file= scanout)
+    scanout.close()
+  print ("Look inside the outputs folder for scan result.")
 
 
+# Nmap OS scan function
+def osScan():
+  print (userInput, "selected.\n")
+  ip = input("Please enter an ip address to scan for OS type: ")
+  print(nm.scan(hosts= ip, arguments=" -O")['scan'][ip]['osmatch'][1])
 
+# Nmap Advanced scan function 
+def advancedScan();
 
-while (True):
-    if userInput == "pingScan":
-        pingScan()
-    else:
-        print ("Please specify a type of scan.")
+# Nmap 
+    
+
+  
+
+if userInput == "pingScan":
+  pingScan()
+elif userInput == "osScan":
+  osScan()
+else:
+  print ("Please specify a correct type of scan.")
